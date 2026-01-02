6b24be608a814a7105c361ed76df9b2ef8ebc81f
a_mdne <129204408+armadne@users.noreply.github.com>
2026-01-02 20:20:20 +0100

    Add files via upload

---
+++ BACKEND/controllers/user.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken')
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+
+    const valid = await bcrypt.compare(req.body.password, user.password);
+
+    if (!valid) {
+      return res.status(401).json({ error: 'Mot de passe incorrect !' });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' }
+    );
+
+    res.status(200).json({
+      userId: user._id,
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
